090cecffe05e1890625338a09b5c045d48f862a7
nishiyamataichi <nishiyama20010724@gmail.com>
2024-10-07 11:01:25 +0900

    first commit

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-Route::get('/posts', [PostController::class, 'index']); 
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,7 +12,10 @@ Route::get('/posts', [PostController::class, 'index']);
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class ,'create']);
+Route::get('/posts/{post}', [PostController::class ,'show']);
+Route::post('/posts', [PostController::class ,'store']);
+Route::get('/posts/{post}/edit', [PostController::class ,'edit']);
+Route::put('/posts/{post}', [PostController::class ,'update']);
+Route::delete('/posts/{post}', [PostController::class,'delete']);
